e98011b8ede192dbe85a1db17845be355a62b4fc
soyelmejorjajaka <adrian.teransanchez@iesmiguelherrero.com>
2024-02-23 18:00:02 +0100

    xdsf<

---
+++ fd/src/main/java/com/mycompany/fd/Fd.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fd;
+
+/**
+ *
+ * @author DAW109
+ */
+public class Fd {
+
+    public static void main(String[] args) {
+        System.out.println("Hello hyaerrrrrqw5a5rhyald!");
+    }
+}
